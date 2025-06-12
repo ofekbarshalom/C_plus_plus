1f62ec87e705fa6ad9fff8cc1a7416b06a825d3b
ofekbarshalom <ofekbarshalom@gmail.com>
2025-06-12 23:26:29 +0300

    b

---
+++ OS_Final_Ex/Part7/GraphAlgorithms.hpp
@@ -0,0 +1,16 @@
+#ifndef GRAPH_ALGORITHMS_HPP
+#define GRAPH_ALGORITHMS_HPP
+
+using namespace std;
+
+#include "Graph.hpp"
+#include <vector>
+
+namespace GraphAlgo {
+
+    bool isEulerian(const Graph& g);
+    vector<int> findEulerianCircuit(const Graph& g);
+
+} // namespace GraphAlgo
+
+#endif
